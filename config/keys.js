2697937b8fe818762949071637d00bf644d2b9a6
Ryan Silva <RyandotSilva@gmail.com>
2019-04-13 11:55:05 -0400

    Add token logic

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
 	mongoURI:
-		'mongodb+srv://ryan:ryan@devconnector-y0efo.mongodb.net/test?retryWrites=true'
+		'mongodb+srv://ryan:ryan@devconnector-y0efo.mongodb.net/test?retryWrites=true',
+	secretOrKey: 'secret'
 };

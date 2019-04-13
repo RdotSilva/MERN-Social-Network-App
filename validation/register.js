b5f8c6c9127293b11ec40cba052d6593b4e26b1b
Ryan Silva <RyandotSilva@gmail.com>
2019-04-13 13:12:08 -0400

    Add password validation

--- validation/register.js
+++ validation/register.js
@@ -6,6 +6,7 @@ module.exports = function validateRegisterInput(data) {
 
 	data.name = !isEmpty(data.name) ? data.name : '';
 	data.email = !isEmpty(data.email) ? data.email : '';
+	data.password = !isEmpty(data.password) ? data.password : '';
 
 	if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
 		errors.name = 'Name must be between 2 and 30 characters';

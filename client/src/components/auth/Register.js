c8dcfda07f87eedd4de586a3633d1c0d39bad257
Ryan Silva <ryandotsilva@gmail.com>
2019-04-15 16:02:22 -0400

    Add state

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: '',
+			email: '',
+			password: '',
+			password2: '',
+			errors: {}
+		};
+	}
+
 	render() {
 		return (
 			<div className="register">

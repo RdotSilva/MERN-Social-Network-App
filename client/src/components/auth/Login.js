d841d6a5bbcd0fc6360ac38cf4edad08e96d8a9e
Ryan Silva <ryandotsilva@gmail.com>
2019-04-15 16:21:15 -0400

    Add constructor

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+	constructor() {
+		super();
+		this.state = {
+			email: '',
+			password: '',
+			errors: {}
+		};
+	}
+
 	render() {
 		return (
 			<div className="login">

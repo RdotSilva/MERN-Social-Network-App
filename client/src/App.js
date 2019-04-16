8dadfc5ce9a15bcc0cd04dad90b05b268325d3b4
Ryan Silva <ryandotsilva@gmail.com>
2019-04-16 17:09:00 -0400

    Add store.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -9,6 +10,8 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 import './App.css';
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
 	render() {

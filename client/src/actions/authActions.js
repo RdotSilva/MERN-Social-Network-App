bae51e83787f55fce698458d7795b194bb6a2a0f
Ryan Silva <ryandotsilva@gmail.com>
2019-04-16 18:02:00 -0400

    Remove axios

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,5 @@
 import { TEST_DISPATCH } from './types';
+import axios from 'axios';
 
 // Register User
-export const registerUser = userData => {
-	return {
-		type: TEST_DISPATCH,
-		payload: userData
-	};
-};
+export const registerUser = userData => {};

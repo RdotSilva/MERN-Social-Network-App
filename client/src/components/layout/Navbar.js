713c82a9d824469d3d2c5c148da27e6f42b887a5
Ryan Silva <ryandotsilva@gmail.com>
2019-04-16 20:05:29 -0400

    Add guestLinks jsx

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -23,6 +23,21 @@ class Navbar extends Component {
 			</ul>
 		);
 
+		const guestLinks = (
+			<ul className="navbar-nav ml-auto">
+				<li className="nav-item">
+					<Link className="nav-link" to="/register">
+						Sign Up
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link className="nav-link" to="/login">
+						Login
+					</Link>
+				</li>
+			</ul>
+		);
+
 		return (
 			<nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
 				<div className="container">

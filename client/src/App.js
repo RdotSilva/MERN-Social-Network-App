f8e406fc0c869269c038b3498b2c96766b7d3544
Ryan Silva <ryandotsilva@gmail.com>
2019-04-18 18:08:27 -0400

    Add clearCurrentProfile to expired token logic

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -31,6 +32,7 @@ if (localStorage.jwtToken) {
 		// Logout User
 		store.dispatch(logoutUser());
 		// Clear current profile
+		store.dispatch(clearCurrentProfile());
 		// Redirect to login
 		window.location.href = '/login';
 	}

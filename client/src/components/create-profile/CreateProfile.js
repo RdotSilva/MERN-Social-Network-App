aa1dd96e770ab3a86c701c9f2f47378b24334e6d
Ryan Silva <ryandotsilva@gmail.com>
2019-04-19 16:25:25 -0400

    Add state keys

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -3,6 +3,27 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class CreateProfile extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			displaySocialInputs: false,
+			handle: '',
+			company: '',
+			website: '',
+			location: '',
+			status: '',
+			skills: '',
+			githubusername: '',
+			bio: '',
+			twitter: '',
+			facebook: '',
+			linkedin: '',
+			youtube: '',
+			instagram: '',
+			errors: {}
+		};
+	}
+
 	render() {
 		return <div />;
 	}

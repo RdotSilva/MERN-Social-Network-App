b8e19a6e04b8a60175ed84f4f1b80b9c0a833796
Ryan Silva <RyandotSilva@gmail.com>
2019-04-20 12:28:46 -0400

    Add componentWillReceiveProps to AddExperience

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -22,6 +22,12 @@ class AddExperience extends Component {
 		};
 	}
 
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.errors) {
+			this.setState({ errors: nextProps.errors });
+		}
+	}
+
 	onSubmit = e => {
 		e.preventDefault();
 

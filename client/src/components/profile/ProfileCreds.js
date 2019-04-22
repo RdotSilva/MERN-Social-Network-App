550b2566a385298b72d7f094b824286efc8f5b82
Ryan Silva <RyandotSilva@gmail.com>
2019-04-22 15:46:02 -0400

    Add displays for education and experience

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -70,8 +70,24 @@ class ProfileCreds extends Component {
 		));
 
 		return (
-			<div>
-				<h1>TODO: Profile Creds</h1>
+			<div className="row">
+				<div className="col-md-6">
+					<h3 className="text-center text-info">Experience</h3>
+					{expItems.length > 0 ? (
+						<ul className="list-group">{expItems}</ul>
+					) : (
+						<p className="text-center">No Experience Listed</p>
+					)}
+				</div>
+
+				<div className="col-md-6">
+					<h3 className="text-center text-info">Education</h3>
+					{eduItems.length > 0 ? (
+						<ul className="list-group">{eduItems}</ul>
+					) : (
+						<p className="text-center">No Education Listed</p>
+					)}
+				</div>
 			</div>
 		);
 	}

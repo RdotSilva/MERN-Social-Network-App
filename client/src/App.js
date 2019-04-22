d9664ad9536ad4b50c6e9f4c285eb65f0fd39d7c
Ryan Silva <RyandotSilva@gmail.com>
2019-04-22 16:17:46 -0400

    Add NotFound route to main App

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -106,6 +107,11 @@ class App extends Component {
 									component={AddEducation}
 								/>
 							</Switch>
+							<Route
+								exact
+								path="/not-found"
+								component={NotFound}
+							/>
 						</div>
 						<Footer />
 					</div>

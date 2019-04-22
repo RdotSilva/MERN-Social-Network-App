73d0490fa2077f362742c96a7a3ac4b9f8baeaf5
Ryan Silva <RyandotSilva@gmail.com>
2019-04-22 15:41:20 -0400

    Add eduItems jsx markup to ProfileCreds

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -39,6 +39,36 @@ class ProfileCreds extends Component {
 			</li>
 		));
 
+		const eduItems = education.map(edu => (
+			<li key={edu._id} className="list-group-item">
+				<h4>{edu.school}</h4>
+				<p>
+					<Moment format="YYYY/MM/DD">{edu.from}</Moment> - -{' '}
+					{edu.to === null ? (
+						' Now'
+					) : (
+						<Moment format="YYYY/MM/DD">{edu.from}</Moment>
+					)}
+				</p>
+				<p>
+					<strong>Degree:</strong>
+					{edu.degree}
+				</p>
+				<p>
+					<strong>Field Of Study:</strong>
+					{edu.fieldofstudy}
+				</p>
+				<p>
+					{edu.description === '' ? null : (
+						<span>
+							<strong>Description: </strong>
+							{edu.description}
+						</span>
+					)}
+				</p>
+			</li>
+		));
+
 		return (
 			<div>
 				<h1>TODO: Profile Creds</h1>

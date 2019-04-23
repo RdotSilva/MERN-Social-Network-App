5353e50918c1a0c10200c3d1acc7bf62f4289809
Ryan Silva <ryandotsilva@gmail.com>
2019-04-23 17:59:22 -0400

    Set up Posts class component

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import PostForm from './PostForm';
+import Spinner from '../common/Spinner';
+
+class Posts extends Component {
+	render() {
+		return <div />;
+	}
+}
+
+export default Posts;

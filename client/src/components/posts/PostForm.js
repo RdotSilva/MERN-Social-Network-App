a93461d00f869ebfe88b1af3055cfc387dc766bc
Ryan Silva <ryandotsilva@gmail.com>
2019-04-23 18:15:50 -0400

    Add jsx markup for PostForm

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -11,9 +11,47 @@ class PostForm extends Component {
 			text: '',
 			errors: {}
 		};
+		// this.onChange = this.onChange.bind(this);
+		// this.onSubmit = this.onSubmit.bind(this);
 	}
+
+	onSubmit = e => {
+		e.preventDefault();
+		console.log('submit');
+	};
+
+	onChange = e => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
 	render() {
-		return <div />;
+		const { errors } = this.state;
+
+		return (
+			<div className="post-form mb-3">
+				<div className="card card-info">
+					<div className="card-header bg-info text-white">
+						Say Somthing...
+					</div>
+					<div className="card-body">
+						<form onSubmit={this.onSubmit}>
+							<div className="form-group">
+								<TextAreaFieldGroup
+									placeholder="Create a Post"
+									name="text"
+									value={this.state.text}
+									onChange={this.onChange}
+									errors={errors.text}
+								/>
+							</div>
+							<button type="submit" className="btn btn-dark">
+								Submit
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
 

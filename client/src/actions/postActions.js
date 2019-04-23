d4cd2bf9c99d21e5249d480e8ca1838f0e6c09ac
Ryan Silva <ryandotsilva@gmail.com>
2019-04-23 18:38:04 -0400

    Add getPosts action

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,9 +1,27 @@
 import axios from 'axios';
 
-import { ADD_POST, GET_ERRORS } from './types';
+import { ADD_POST, GET_ERRORS, GET_POSTS } from './types';
 
 // Add Post
 export const addPost = postData => dispatch => {
+	axios
+		.post('/api/posts', postData)
+		.then(res =>
+			dispatch({
+				type: ADD_POST,
+				payload: res.data
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
+// Get Posts
+export const getPosts = () => dispatch => {
 	axios
 		.post('/api/posts', postData)
 		.then(res =>

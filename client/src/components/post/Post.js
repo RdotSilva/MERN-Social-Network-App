0262ada01091b0c78c4610681c8ed371ed748433
Ryan Silva <ryandotsilva@gmail.com>
2019-04-24 16:57:16 -0400

    Add postContent logic

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import PostItem from '../posts/PostItem';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
 
@@ -10,6 +11,18 @@ class Post extends Component {
 	}
 
 	render() {
+		const { post, loading } = this.props;
+		let postContent;
+
+		if (post === null || loading || Object.keys(post).length === 0) {
+			postContent = <Spinner />;
+		} else {
+			postContent = (
+				<div>
+					<PostItem post={post} />
+				</div>
+			);
+		}
 		return (
 			<div>
 				<h1>POST</h1>

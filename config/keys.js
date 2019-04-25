098c3dbd2c140bc89df2d0fba90085d3bd2ebc72
Ryan Silva <ryandotsilva@gmail.com>
2019-04-25 15:50:02 -0400

    Add logic for for using keys file

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-	mongoURI:
-		'mongodb+srv://ryan:ryan@devconnector-y0efo.mongodb.net/test?retryWrites=true',
-	secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./keys_prod');
+} else {
+	module.exports = require('./keys_dev');
+}

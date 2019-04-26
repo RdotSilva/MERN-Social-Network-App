88b2008afe88684dc67513ccda0db8158e64c12e
Ryan Silva <ryandotsilva@gmail.com>
2019-04-26 09:26:16 -0400

    Fix store.js file

--- client/src/store.js
+++ client/src/store.js
@@ -6,18 +6,14 @@ const initialState = {};
 
 const middleware = [thunk];
 
-// Developer tools middleware
-const composeSetup =
-	process.env.NODE_ENV !== 'production' &&
-	typeof window === 'object' &&
-	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-		: compose;
-
 const store = createStore(
 	rootReducer,
 	initialState,
-	composeSetup(applyMiddleware(...middleware))
+	compose(
+		applyMiddleware(...middleware),
+		window.__REDUX_DEVTOOLS_EXTENSION__ &&
+			window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
 );
 
 export default store;

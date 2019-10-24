5ca9806f4021c8c219e5ef66163ac20d00adf4e1
Ryan Silva <RyandotSilva@gmail.com>
2019-10-23 22:24:54 -0400

    Change check icon to tool icon for skills

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import isEmpty from '../../validation/is-empty';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileAbout extends Component {
 	render() {
 		const { profile } = this.props;
 
 		// Get first name
-		const firstName = profile.user.name.trim().split(' ')[0];
+		const firstName = profile.user.name.trim().split(" ")[0];
 
 		// Skill List
 		const skills = profile.skills.map((skill, index) => (
-			<div key={index} className="p-3">
-				<i className="fa fa-check" /> {skill}
+			<div key={index} className="p-3 ml-5">
+				<i className="fa fa-tools " /> {skill}
 			</div>
 		));
 
@@ -20,10 +20,8 @@ class ProfileAbout extends Component {
 			<div className="row">
 				<div className="col-md-12">
 					<div className="card card-body bg-light mb-3">
-						<h3 className="text-center text-info">
-							{firstName}'s Bio
-						</h3>
-						<p className="lead">
+						<h3 className="text-center text-info">{firstName}'s Bio</h3>
+						<p className="lead text-center">
 							{isEmpty(profile.bio) ? (
 								<span>{firstName} does not have a bio</span>
 							) : (
--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import Moment from 'react-moment';
+import React, { Component } from "react";
+import Moment from "react-moment";
 
 class ProfileCreds extends Component {
 	render() {
@@ -9,9 +9,9 @@ class ProfileCreds extends Component {
 			<li key={exp._id} className="list-group-item">
 				<h4>{exp.company}</h4>
 				<p>
-					<Moment format="YYYY/MM/DD">{exp.from}</Moment> - -{' '}
+					<Moment format="YYYY/MM/DD">{exp.from}</Moment> - -{" "}
 					{exp.to === null ? (
-						' Now'
+						" Now"
 					) : (
 						<Moment format="YYYY/MM/DD">{exp.from}</Moment>
 					)}
@@ -21,7 +21,7 @@ class ProfileCreds extends Component {
 					{exp.title}
 				</p>
 				<p>
-					{exp.location === '' ? null : (
+					{exp.location === "" ? null : (
 						<span>
 							<strong>Location: </strong>
 							{exp.location}
@@ -29,7 +29,7 @@ class ProfileCreds extends Component {
 					)}
 				</p>
 				<p>
-					{exp.description === '' ? null : (
+					{exp.description === "" ? null : (
 						<span>
 							<strong>Description: </strong>
 							{exp.description}
@@ -43,9 +43,9 @@ class ProfileCreds extends Component {
 			<li key={edu._id} className="list-group-item">
 				<h4>{edu.school}</h4>
 				<p>
-					<Moment format="YYYY/MM/DD">{edu.from}</Moment> - -{' '}
+					<Moment format="YYYY/MM/DD">{edu.from}</Moment> - -{" "}
 					{edu.to === null ? (
-						' Now'
+						" Now"
 					) : (
 						<Moment format="YYYY/MM/DD">{edu.from}</Moment>
 					)}
@@ -59,7 +59,7 @@ class ProfileCreds extends Component {
 					{edu.fieldofstudy}
 				</p>
 				<p>
-					{edu.description === '' ? null : (
+					{edu.description === "" ? null : (
 						<span>
 							<strong>Description: </strong>
 							{edu.description}

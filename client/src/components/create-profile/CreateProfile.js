3dc7a1ae34797580d2b744be42741cfcaaf6ace1
Ryan Silva <RyandotSilva@gmail.com>
2019-10-23 22:52:24 -0400

    Update profile to get gender

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,31 +1,32 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import TextFieldGroup from '../common/TextFieldGroup';
-import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
-import InputGroup from '../common/InputGroup';
-import SelectListGroup from '../common/SelectListGroup';
-import { createProfile } from '../../actions/profileActions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import InputGroup from "../common/InputGroup";
+import SelectListGroup from "../common/SelectListGroup";
+import { createProfile } from "../../actions/profileActions";
 
 class CreateProfile extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			displaySocialInputs: false,
-			handle: '',
-			company: '',
-			website: '',
-			location: '',
-			status: '',
-			skills: '',
-			githubusername: '',
-			bio: '',
-			twitter: '',
-			facebook: '',
-			linkedin: '',
-			youtube: '',
-			instagram: '',
+			handle: "",
+			company: "",
+			website: "",
+			location: "",
+			status: "",
+			skills: "",
+			githubusername: "",
+			bio: "",
+			sex: "",
+			twitter: "",
+			facebook: "",
+			linkedin: "",
+			youtube: "",
+			instagram: "",
 			errors: {}
 		};
 	}
@@ -48,6 +49,7 @@ class CreateProfile extends Component {
 			skills: this.state.skills,
 			githubusername: this.state.githubusername,
 			bio: this.state.bio,
+			sex: this.state.sex,
 			twitter: this.state.twitter,
 			facebook: this.state.facebook,
 			linkedin: this.state.linkedin,
@@ -118,40 +120,40 @@ class CreateProfile extends Component {
 		// Select options for status
 		const options = [
 			{
-				label: '* Select Professional Status',
+				label: "* Select Professional Status",
 				value: 0
 			},
 			{
-				label: 'Developer',
-				value: 'Developer'
+				label: "Developer",
+				value: "Developer"
 			},
 			{
-				label: 'Junior Developer',
-				value: 'Junior Developer'
+				label: "Junior Developer",
+				value: "Junior Developer"
 			},
 			{
-				label: 'Senior Developer',
-				value: 'Senior Developer'
+				label: "Senior Developer",
+				value: "Senior Developer"
 			},
 			{
-				label: 'Manager',
-				value: 'Manager'
+				label: "Manager",
+				value: "Manager"
 			},
 			{
-				label: 'Student or Learning',
-				value: 'Student or Learning'
+				label: "Student or Learning",
+				value: "Student or Learning"
 			},
 			{
-				label: 'Instructor or Teacher',
-				value: 'Instructor or Teacher'
+				label: "Instructor or Teacher",
+				value: "Instructor or Teacher"
 			},
 			{
-				label: 'Intern',
-				value: 'Intern'
+				label: "Intern",
+				value: "Intern"
 			},
 			{
-				label: 'Other',
-				value: 'Other'
+				label: "Other",
+				value: "Other"
 			}
 		];
 
@@ -160,16 +162,11 @@ class CreateProfile extends Component {
 				<div className="container">
 					<div className="row">
 						<div className="col-md-8 m-auto">
-							<h1 className="display-4 text-center">
-								Create Your Profile
-							</h1>
+							<h1 className="display-4 text-center">Create Your Profile</h1>
 							<p className="lead text-center">
-								Let's get some information to make your
-								profilestand out
+								Let's get some information to make your profilestand out
 							</p>
-							<small className="d-block pb-3">
-								* = required fields
-							</small>
+							<small className="d-block pb-3">* = required fields</small>
 							<form onSubmit={this.onSubmit}>
 								<TextFieldGroup
 									placeholder="* Profile Handle"
@@ -236,6 +233,14 @@ class CreateProfile extends Component {
 									onChange={this.onChange}
 									error={errors.bio}
 									info="Tell us a little about yourself"
+								/>
+								<TextFieldGroup
+									placeholder="Sex"
+									name="sex"
+									value={this.state.sex}
+									onChange={this.onChange}
+									error={errors.sex}
+									info="Please enter your gender"
 								/>
 								<div className="mb-">
 									<button
--- models/Profile.js
+++ models/Profile.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
 const ProfileSchema = new Schema({
 	user: {
 		type: Schema.Types.ObjectId,
-		ref: 'users'
+		ref: "users"
 	},
 	handle: {
 		type: String,
@@ -31,6 +31,10 @@ const ProfileSchema = new Schema({
 	},
 	bio: {
 		type: String
+	},
+	sex: {
+		type: String,
+		required: true
 	},
 	githubusername: {
 		type: String
@@ -117,4 +121,4 @@ const ProfileSchema = new Schema({
 	}
 });
 
-module.exports = Profile = mongoose.model('profile', ProfileSchema);
+module.exports = Profile = mongoose.model("profile", ProfileSchema);

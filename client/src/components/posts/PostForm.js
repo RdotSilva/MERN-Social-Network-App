3eb4d99f58a661ca39f78c23bddb0e574e966c09
Ryan Silva <RyandotSilva@gmail.com>
2019-10-23 22:35:01 -0400

    Update post form

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
-import { addPost } from '../../actions/postActions';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import { addPost } from "../../actions/postActions";
 
 class PostForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			text: '',
+			text: "",
 			errors: {}
 		};
 		this.onChange = this.onChange.bind(this);
@@ -33,7 +33,7 @@ class PostForm extends Component {
 		};
 
 		this.props.addPost(newPost);
-		this.setState({ text: '' });
+		this.setState({ text: "" });
 	}
 
 	onChange(e) {
@@ -46,9 +46,7 @@ class PostForm extends Component {
 		return (
 			<div className="post-form mb-3">
 				<div className="card card-info">
-					<div className="card-header bg-info text-white">
-						Say Somthing...
-					</div>
+					<div className="card-header bg-info text-white">What do you say?</div>
 					<div className="card-body">
 						<form onSubmit={this.onSubmit}>
 							<div className="form-group">
